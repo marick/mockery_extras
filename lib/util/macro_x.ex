e793dd620725a1eb2916d7d89666902f27f0dae2
Brian Marick <marick@exampler.com>
2021-03-31 16:36:43 -0500

    Add docstring

--- lib/util/macro_x.ex
+++ lib/util/macro_x.ex
@@ -1,13 +1,32 @@
 defmodule MockeryExtras.MacroX do
 
-  @moduledoc false
+  @doc """
+  A function that takes two forms of function call and returns their
+  constitutent parts. The forms are syntax trees (typically given to a
+  macro that uses the results of this function.)
 
-    # Note that this doesn't resolve aliases, which requires the __ENV__ at
-    # macroexpansion time. Haven't figured out how to make use of that
-    # information within a single function, so the caller will have to
-    # adjust the result if the first part of the function is a module
-    # alias.
-  
+  1. The first form is like `Module.function(1, 2, 3)`. In this case, the
+     following structure is returned:
+
+          {:in_named_module, Module, [function: 3], [1, 2, 3]}
+
+     The module part of the function call may be multi-part, like
+     `Module.Submodule`. That's what will appear in the second tuple
+     position. The module may be the use of an alias, like `alias
+     Module.Submodule, as: S`. In that case, the alias (`S`) will appear
+     in the second position.
+
+  2. The second form has no module component, like `function(1, 2, 3)`, which
+     returns:
+
+          {:in_calling_module, :use__MODULE__, [function: 3], [1, 2, 3]}
+
+     This value signals that the caller must supply the module. If the caller
+     is a macro, it will probably use `__MODULE__`.
+
+  Any other form will raise `RuntimeError`.
+  """
+
   def decompose_call_alt(funcall) do
     case Macro.decompose_call(funcall) do
       {{:__aliases__, _, aliases},  fun_atom, args} -> 
